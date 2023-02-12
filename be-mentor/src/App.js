0c2946b9ea5e3466714e66c5910e095311ad8264
DikshaVerma25 <dikshaverma235@gmail.com>
2023-02-11 20:19:38 -0500

    format

--- be-mentor/src/App.js
+++ be-mentor/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Navbar  from './components/navbar.js';
-import Collage from './components/identityphoto/collage';
 import TextFields from './components/textfield';
 import Checkbox from './components/checkbox';
 
@@ -16,7 +15,11 @@ function App() {
      
      <h1 style = {{textAlign : 'left', margin: 88 }}> Career Field </h1>
      <Checkbox />
-     
+     <button style={{ marginLeft: 85 , display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', margin: '20px 0',  width: 100, height: 40, backgroundColor: 'red', color: '#fff', border: 'none', borderRadius: 5,  fontSize: 16, cursor: 'pointer', textAlign: 'center' }}
+>
+   Submit
+</button>
+
       <header className="App-header">   
       </header>
     </div>
--- be-mentor/src/components/navbar.js
+++ be-mentor/src/components/navbar.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './navbar.css';
 import logo from './identityphoto/photos/logo.png';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo" />
       <ul>
         <li>
-          <a href="#">Home</a>
+          <a href = "https://edfoundationac.org/ ">Home<link /></a> 
         </li>
         <li>
-          <a href="#">About</a>
+          <a href=" https://edfoundationac.org/about-the-education-foundation/">About</a>
         </li>
         <li>
-          <a href="#">Contact</a>
+          <a href="https://edfoundationac.org/contact-us/">Contact</a>
         </li>
       </ul>
     </nav>
