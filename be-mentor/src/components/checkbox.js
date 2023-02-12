5de87d2c5a1433c300b5a9749e7dba29b1ff2a45
DikshaVerma25 <dikshaverma235@gmail.com>
2023-02-11 20:22:03 -0500

    check-box

--- be-mentor/src/components/checkbox.js
+++ be-mentor/src/components/checkbox.js
@@ -31,27 +31,65 @@ function Checkbox() {
   };
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-    <form onSubmit={handleSubmit}>
-      <input
-        type="checkbox"
-        name="item1"
-        onChange={handleChange}
-        />
-        <p>Agriculture, Food, and Natural Resources</p>
-      <input
-        type="checkbox"
-        name="item2"
-        onChange={handleChange}
-      />
-      <input
-        type="checkbox"
-        name="item3"
-        onChange={handleChange}
-      />
-    </form>
-    </div>
-  );
-}
+    <div style={{ marginLeft: 8, width: 100, display: 'flex' }}> 
+      <form onSubmit={handleSubmit}>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'left' }}>
+          <input
+            type="checkbox"
+            name="item1"
+            onChange={handleChange}
+          /> Agriculture, Food, and Natural Resources
+          </div>
+        
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <input
+            type="checkbox"
+            name="item2"
+            onChange={handleChange}
+          /> </div>
+          <p style={{ margin: 0, paddingLeft: 8 }}>Architecture and Construction</p>
+       
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <input
+            type="checkbox"
+            name="item3"
+            onChange={handleChange}
+          /></div>
+          <p style={{ margin: 0, paddingLeft: 8 }}>Arts, Audio/Video Technology and Communications</p>
+        
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <input
+            type="checkbox"
+            name="item4"
+            onChange={handleChange}
+          />Business, Management, and Administration</div>
+          <br/>
+        
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <input
+            type="checkbox"
+            name="item3"
+            onChange={handleChange}
+          /></div>
+          <p style={{ margin: 0, paddingLeft: 8 }}>Education and Training</p>
+        
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <input
+            type="checkbox"
+            name="item6"
+            onChange={handleChange}
+            ></input>
+            </div>
+            <p style={{ margin: 0, paddingLeft: 8 }}>Manufacturing</p>
+            </form>
+            
+        </div>
+    
 
-export default Checkbox;
+);
+};
+
+export default Checkbox ; 
+
+
+    
